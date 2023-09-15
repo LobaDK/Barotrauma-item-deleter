f2b33722f34c5a3adb18eb20ccddee9a3cbdc042
LobaDK <lobadk@lobadk.com>
2023-09-15 21:54:49 +0200

    Finished?

--- Baro item deleter.py
+++ Baro item deleter.py
@@ -6,6 +6,25 @@ import logging
 from tqdm import tqdm
 from time import perf_counter
 
+submarine_input = Path(input("Drag and drop .sub file into this window to quickly and easily specify it's location and filename: "))
+if str(submarine_input).startswith("'") or str(submarine_input).startswith('"'):
+    submarine_input = Path(str(submarine_input)[1:-1])
+if str(submarine_input).endswith("'") or str(submarine_input).endswith('"'):
+    submarine_input = Path(str(submarine_input)[:-1])
+
+if not Path('warningacknowledged').exists():
+    warninginput = input("\nWARNING: While the program won't delete or mess with any of the original files "
+                         "it will recursively delete any and all items that are in containers, "
+                         "and items that are inside those items, and so on, until it can't find any more items. "
+                         "While it is limited to certain container types, I cannot guarantee that it won't delete anything important."
+                         "\n\nWrite 'yes' to acknowledge and dissmiss this warning: ")
+
+    if warninginput.lower() == 'yes':
+        Path('warningacknowledged').touch()
+    else:
+        input("'yes' was not written, press enter to exit.")
+        exit()
+
 start = perf_counter()
 
 logging.basicConfig(filename='log.txt', filemode='w', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -30,7 +49,15 @@ containers_and_items = ['steelcabinet',
                         'chaingunloader',
                         'pulselaserloader',
                         'outpostreactor',
-                        'reactor1']
+                        'reactor1',
+                        'battery',
+                        'oxygenerator',
+                        'opdeco_medcompartment1',
+                        'opdeco_medcompartment2',
+                        'opdeco_medcompartment3',
+                        'opdeco_cabinetsdorm',
+                        'loosevent',
+                        'opdeco_trashcan']
 
 item_ids = []
 
@@ -43,6 +70,9 @@ def recursive_search(item_id, items):
                 items.set_description(f'Scanning {child.attrib["identifier"]}')
                 for container in child:
                     if container.tag == 'ItemContainer':
+                        logging.info(f'Found container: {child.attrib["identifier"]} '
+                                     f'with item IDs: {container.attrib["contained"] if container.attrib["contained"] else "None"} '
+                                     f'from item with ID: {item_id}')
                         inspection_ids = container.attrib['contained'].split(',')
                         for item in inspection_ids:
                             if item == '':
@@ -58,11 +88,15 @@ def recursive_search(item_id, items):
                             recursive_search(item, items)
 
 
-if Path(r'Metallicor MK1.xml').exists():
-    Path(r'Metallicor MK1.xml').unlink()
+if Path(submarine_input.parent / (f'{submarine_input.stem}.xml')).exists():
+    logging.info('Deleting already existing file')
+    print('Deleting already existing file')
+    Path(submarine_input.parent / (f'{submarine_input.stem}.xml')).unlink()
 
-with gzip.open(r'Metallicor MK1.sub', 'rb') as f_in:
-    with open(r'Metallicor MK1.xml', 'wb') as f_out:
+logging.info('Extracting sub file')
+print('Extracting sub file')
+with gzip.open(submarine_input, 'rb') as f_in:
+    with open(Path(submarine_input.parent / (f'{submarine_input.stem}.xml')), 'wb') as f_out:
         shutil.copyfileobj(f_in, f_out)
 
 tree = ET.parse('Metallicor MK1.xml')
@@ -74,6 +108,7 @@ for child in root.iter():
         if child.attrib['identifier'] in containers_and_items:
             for container in child:
                 if container.tag == 'ItemContainer':
+                    logging.info(f'Found container: {child.attrib["identifier"]} with item IDs: {container.attrib["contained"]}')
                     inspection_ids = container.attrib['contained'].split(',')
                     for item in inspection_ids:
                         if ';' in item:
@@ -83,18 +118,14 @@ for child in root.iter():
 
 item_list = [i for i in item_ids if i]
 
-print(item_list)
-print(len(item_list))
-
 items = tqdm(item_list)
 for item in items:
-    items.bar_format = '{l_bar}{bar:10}| {n_fmt}/{total_fmt} [{elapsed}<{remaining}]'
+    logging.info(f'Recursively searching item with ID: {item}')
     recursive_search(item, items)
 
 final_item_list = [i for i in item_ids if i]
 final_item_list = list(set(final_item_list + item_list))
 final_item_list.sort()
-print(final_item_list)
 
 i = 0
 for child in root.findall('Item'):
@@ -102,10 +133,11 @@ for child in root.findall('Item'):
         if child.attrib['ID'] in final_item_list:
             root.remove(child)
             i += 1
-            logging.info(f'Removed {child.attrib["ID"]} which is {child.attrib["identifier"]}')
-            print(f'Removed {child.attrib["ID"]} which is {child.attrib["identifier"]}')
+            logging.info(f'Removed item with ID: {child.attrib["ID"]} ({child.attrib["identifier"]})')
+            print(f'Removed item with ID: {child.attrib["ID"]} ({child.attrib["identifier"]})')
 
-
+logging.info('Resetting container contained values')
+print('Resetting container contained values')
 for child in root.iter():
     if child.tag == 'Item':
         if child.attrib['identifier'] in containers_and_items:
@@ -115,14 +147,26 @@ for child in root.iter():
                     container.attrib['contained'] = ',' * commas
 
 
-tree.write('Metallicor MK1 test.xml')
+logging.info('Saving edited XML file')
+print('Saving edited XML file')
+root.attrib['name'] = f'{submarine_input.stem} (no items)'
+tree.write(Path(submarine_input.parent / (f'{submarine_input.stem} (no items).xml')))
 
-with open('Metallicor MK1 test.xml', 'rb') as f_in:
-    with gzip.open('Metallicor MK1 test', 'wb') as f_out:
+logging.info('Compressing XML file back into gzip .sub file')
+print('Compressing XML file back into gzip .sub file')
+with open(Path(submarine_input.parent / (f'{submarine_input.stem} (no items).xml')), 'rb') as f_in:
+    with gzip.open(Path(submarine_input.parent / (f'{submarine_input.stem} (no items)')), 'wb') as f_out:
         shutil.copyfileobj(f_in, f_out)
 
-if Path('Metallicor MK1 test.sub').exists():
-    Path('Metallicor MK1 test.sub').unlink()
-Path('Metallicor MK1 test').rename('Metallicor MK1 test.sub')
+if Path(Path(submarine_input.parent / (f'{submarine_input.stem} (no items).sub'))).exists():
+    logging.info('Deleting already existing .sub file')
+    print('Deleting already existing .sub file')
+    Path(Path(submarine_input.parent / (f'{submarine_input.stem} (no items).sub'))).unlink()
 
+logging.info('Renaming .sub file')
+print('Renaming .sub file')
+Path(Path(submarine_input.parent / (f'{submarine_input.stem} (no items)'))).rename(Path(submarine_input.parent / (f'{submarine_input.stem} (no items).sub')))
+
+logging.info(f'Removed {i} items and saved edited file. Took {perf_counter() - start} seconds.')
 print(f'Removed {i} items and saved edited file. Took {perf_counter() - start} seconds.')
+input('Press enter to exit.')
